138dabc887b30c26ce4bd923a76c8c7906f33791
Bohdan Bilyk <bohdanbilyk1001@gmail.com>
2024-11-21 20:12:07 +0100

    init commit

---
+++ src/test/java/com/lubaszka/salarycalculator/SalaryCalculator/SalaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubaszka.salarycalculator.SalaryCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
